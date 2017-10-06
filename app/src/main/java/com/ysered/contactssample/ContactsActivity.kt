4cc72155bbbbd0691f35ebc2c7122fc308bed3b6
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-10-06 13:14:43 +0300

    Created project.

---
+++ app/src/main/java/com/ysered/contactssample/ContactsActivity.kt
@@ -0,0 +1,12 @@
+package com.ysered.contactssample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+    }
+}
