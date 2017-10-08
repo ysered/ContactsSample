bce3cfc245e2aeade9bf69fa93354a2416556751
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-10-08 03:19:10 +0300

    Get rid of extra observer class.

--- app/src/main/java/com/ysered/contactssample/data/Phone.kt
+++ app/src/main/java/com/ysered/contactssample/data/Phone.kt
@@ -1,3 +1,32 @@
 package com.ysered.contactssample.data
 
+import android.content.Context
+import android.database.Cursor
+import android.provider.ContactsContract
+import com.ysered.contactssample.R
+import com.ysered.contactssample.utils.getInt
+import com.ysered.contactssample.utils.getString
+
 data class Phone(val type: String, val number: String)
+
+fun Cursor.getPhone(context: Context): Phone? {
+    val type = getInt(ContactsContract.CommonDataKinds.Phone.TYPE)
+    val typeName = getPhoneTypeName(context, type)
+    return if (typeName != null) {
+        val number = getString(ContactsContract.CommonDataKinds.Phone.NUMBER)
+        Phone(typeName, number)
+    } else
+        null
+}
+
+private fun getPhoneTypeName(context: Context, type: Int): String? {
+    // TODO: add rest of phone types
+    val resourceId = when (type) {
+        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE -> R.string.phone_type_mobile
+        ContactsContract.CommonDataKinds.Phone.TYPE_WORK -> R.string.phone_type_work
+        ContactsContract.CommonDataKinds.Phone.TYPE_HOME -> R.string.phone_type_home
+        ContactsContract.CommonDataKinds.Phone.TYPE_OTHER -> R.string.phone_type_other
+        else -> -1
+    }
+    return if (resourceId != -1) context.getString(resourceId) else null
+}
--- app/src/main/java/com/ysered/contactssample/details/DetailsActivity.kt
+++ app/src/main/java/com/ysered/contactssample/details/DetailsActivity.kt
@@ -1,10 +1,14 @@
 package com.ysered.contactssample.details
 
-import android.arch.lifecycle.Observer
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.support.design.widget.CollapsingToolbarLayout
+import android.support.v4.app.LoaderManager
+import android.support.v4.content.CursorLoader
+import android.support.v4.content.Loader
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -12,16 +16,18 @@ import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.widget.ImageView
 import com.ysered.contactssample.R
-import com.ysered.contactssample.data.Contact
-import com.ysered.contactssample.data.getContactExtra
-import com.ysered.contactssample.data.putExtra
-import com.ysered.contactssample.utils.info
+import com.ysered.contactssample.data.*
+import com.ysered.contactssample.utils.forEach
 import com.ysered.contactssample.utils.showToast
 
 
 class DetailsActivity : AppCompatActivity() {
 
     companion object {
+        private val PHONES_LOADER = 0
+        private val PHONES_SELECTION = "${ContactsContract.CommonDataKinds.Phone.CONTACT_ID} = ?"
+        private val PHONES_SORT_ORDER = ContactsContract.CommonDataKinds.Phone.SORT_KEY_PRIMARY
+
         fun start(context: Context, contact: Contact) {
             val intent = Intent(context, DetailsActivity::class.java).putExtra(contact)
             context.startActivity(intent)
@@ -34,6 +40,41 @@ class DetailsActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
     private lateinit var photoImage: ImageView
     private lateinit var detailsAdapter: DetailsAdapter
+
+    private val loaderCallbacks = object : LoaderManager.LoaderCallbacks<Cursor> {
+        override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> = when (id) {
+            PHONES_LOADER -> {
+                val selectionArgs = arrayOf(contact!!.id)
+                CursorLoader(
+                        this@DetailsActivity,
+                        ContactsContract.Data.CONTENT_URI,
+                        null,
+                        PHONES_SELECTION,
+                        selectionArgs,
+                        PHONES_SORT_ORDER
+                )
+            }
+            else -> throw RuntimeException("Unknown loader id: $id")
+        }
+
+        override fun onLoadFinished(loader: Loader<Cursor>?, cursor: Cursor?) {
+            if (loader != null && cursor != null) {
+                when (loader.id) {
+                    PHONES_LOADER -> {
+                        val phones = mutableListOf<Phone>()
+                        cursor.forEach {
+                            getPhone(this@DetailsActivity)?.let { phones.add(it) }
+                        }
+                        if (phones.isNotEmpty())
+                            detailsAdapter.phones = phones
+                    }
+                    else -> throw RuntimeException("Unknown loader id: ${loader.id}")
+                }
+            }
+        }
+
+        override fun onLoaderReset(loader: Loader<Cursor>?) = Unit
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,14 +122,7 @@ class DetailsActivity : AppCompatActivity() {
 
     private fun initPhonesLoader() {
         if (contact!!.hasPhoneNumber) {
-            val observer = ContactDetailsObserver(this, contact!!.id).apply {
-                phonesData.observe(this@DetailsActivity, Observer { phones ->
-                    info("Received phones: $phones")
-                    phones?.let { detailsAdapter.phones = it }
-                })
-            }
-            lifecycle.addObserver(observer)
-            supportLoaderManager.initLoader(ContactDetailsObserver.PHONES_LOADER, null, observer)
+            supportLoaderManager.initLoader(PHONES_LOADER, null, loaderCallbacks)
         }
     }
 }
